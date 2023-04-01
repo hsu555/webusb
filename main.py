330fcee32e09c5888750261195c1488702c7e154
Steve Hsu <hsu555@users.noreply.github.com>
2023-04-01 14:33:02 +0800

    Update main.blocks, main.py

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ def show_working():
     serial.write_value("acc.y", input.acceleration(Dimension.Y))
     serial.write_value("acc.z", input.acceleration(Dimension.Z))
     led.toggle(randint(0, 4), randint(0, 4))
-    basic.pause(200)
+    control.wait_micros(30000)
 
 def on_button_pressed_a():
     basic.clear_screen()
     serial.write_line("" + str((input.temperature())))
     basic.show_string("" + str((input.temperature())))
-    basic.pause(200)
-    serial.write_line("" + str((input.light_level())))
-    basic.show_string("" + str((input.light_level())))
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    show_working()
+    for index in range(100):
+        show_working()
+    basic.clear_screen()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_logo_pressed():
@@ -24,7 +23,15 @@ def on_logo_pressed():
     basic.pause(200)
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
-music.play_melody("C D E F G A B C5 ", 120)
+music.play_sound_effect(music.create_sound_effect(WaveShape.SINE,
+        5000,
+        0,
+        255,
+        0,
+        1000,
+        SoundExpressionEffect.NONE,
+        InterpolationCurve.LINEAR),
+    SoundExpressionPlayMode.IN_BACKGROUND)
 
 def on_forever():
     pass
